f8d5a5fa5bbecec65ec0bf1391624d8bb372359b
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2023-05-21 15:19:57 +0900

    🚧 btvn 0518_01

--- lib/page/my_app.dart
+++ lib/page/my_app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:zendvn_online/page/bt08.dart';
+import 'package:zendvn_online/page/btvn0518_01.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const BT08();
+    return const BTVN051801();
   }
 }
