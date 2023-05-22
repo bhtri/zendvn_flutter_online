292938b812ef9970d3bf477353293e2e5a139081
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2023-05-22 14:13:42 +0900

    done btvn0518_02

--- lib/page/btvn0518_02.dart
+++ lib/page/btvn0518_02.dart
@@ -68,41 +68,173 @@ class BTVN051802 extends StatelessWidget {
                   borderRadius: BorderRadius.circular(40),
                 ),
                 child: ListView.separated(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: EdgeInsets.zero,
                   itemBuilder: (context, index) {
                     return AspectRatio(
                       aspectRatio: 2,
-                      child: Row(
+                      child: Stack(
                         children: [
-                          Container(
-                            alignment: Alignment.center,
-                            height: double.infinity,
-                            width: 60,
-                            decoration: const BoxDecoration(
-                              color: Color(0xffFD8D61),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(40)),
-                            ),
-                            child: RotatedBox(
-                              quarterTurns: -1,
-                              child: Text(
-                                'Study',
-                                style: GoogleFonts.dancingScript(
-                                  textStyle: const TextStyle(
-                                    fontSize: 28,
-                                    color: Colors.white,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                            child: Row(
+                              children: [
+                                Container(
+                                  alignment: Alignment.center,
+                                  height: double.infinity,
+                                  width: 60,
+                                  decoration: const BoxDecoration(
+                                    color: Color(0xffFD8D61),
+                                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                                  ),
+                                  child: RotatedBox(
+                                    quarterTurns: -1,
+                                    child: Text(
+                                      'Study',
+                                      style: GoogleFonts.dancingScript(
+                                        textStyle: const TextStyle(
+                                          fontSize: 28,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
+                                const SizedBox(width: 20),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      color: Color(0xffFDB99B),
+                                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          height: double.infinity,
+                                          width: 30,
+                                          decoration: const BoxDecoration(
+                                            color: Color(0xffFD8D61),
+                                            borderRadius: BorderRadius.horizontal(left: Radius.circular(20)),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Text(
+                                                  'Hoc tap va lam viec',
+                                                  style: GoogleFonts.dancingScript(
+                                                    textStyle: const TextStyle(
+                                                      fontSize: 30,
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                ),
+                                                const SizedBox(height: 10),
+                                                Text(
+                                                  '8 AM - 1 PM',
+                                                  style: GoogleFonts.dancingScript(
+                                                    textStyle: const TextStyle(
+                                                      fontSize: 20,
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                ),
+                                                const SizedBox(height: 10),
+                                                const Text(
+                                                  'Consequatur maxime nam dolore qui optio molestiae laudantium sequi. Ipsam rerum ut. Ut iste repellendus et praesentium vero. Aut ea est nostrum. Explicabo laboriosam officiis placeat sed cum aut maiores.',
+                                                  maxLines: 3,
+                                                  overflow: TextOverflow.ellipsis,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              color: Colors.amber,
+                          Positioned(
+                            bottom: 0,
+                            right: 10,
+                            child: Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    shadowColor: Colors.transparent,
+                                    foregroundColor: Colors.blue,
+                                    padding: EdgeInsets.zero,
+                                    minimumSize: const Size(50, 40),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  ),
+                                  child: const Icon(
+                                    Icons.workspace_premium,
+                                    size: 30,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    shadowColor: Colors.transparent,
+                                    foregroundColor: Colors.blue,
+                                    padding: EdgeInsets.zero,
+                                    minimumSize: const Size(50, 40),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  ),
+                                  child: const Icon(
+                                    Icons.browse_gallery,
+                                    size: 30,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    shadowColor: Colors.transparent,
+                                    foregroundColor: Colors.blue,
+                                    padding: EdgeInsets.zero,
+                                    minimumSize: const Size(50, 40),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  ),
+                                  child: const Icon(
+                                    Icons.edit,
+                                    size: 30,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    shadowColor: Colors.transparent,
+                                    foregroundColor: Colors.blue,
+                                    padding: EdgeInsets.zero,
+                                    minimumSize: const Size(50, 40),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  ),
+                                  child: const Icon(
+                                    Icons.delete_forever,
+                                    size: 30,
+                                  ),
+                                ),
+                              ],
                             ),
-                          )
+                          ),
                         ],
                       ),
                     );
